d156d8c3af056625c8b0faa1588bb84a81dd988a
Yannick Wenger <y.wenger@gmx.de>
2025-06-01 12:38:55 +0200

    chore: remove rooms-v2 page after migration
    - Remove the experimental rooms-v2 page
    - All functionality now available in database/rooms page

--- frontend/src/app/database/rooms-v2/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { DatabasePage } from "@/components/ui/database";
-import { roomsConfig } from "@/lib/database";
-
-export default function RoomsV2Page() {
-  return <DatabasePage config={roomsConfig} />;
-}
