ef40bec34bdcbc576234127bbdd852a8903e68af
Yannick Wenger <y.wenger@gmx.de>
2025-11-12 15:49:31 +0100

    chore: add husky pre-commit hooks for frontend linting and formatting

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,7 +1,7 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-// Test comment for Husky hook
+// Final test - Hook should run now!
 export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
