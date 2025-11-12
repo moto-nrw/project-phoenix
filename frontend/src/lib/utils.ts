a603677f234d8965f820f6ae7357af771596bc9d
Yannick Wenger <y.wenger@gmx.de>
2025-11-12 15:50:18 +0100

    chore: remove test changes from utils and README

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,7 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-// Final test - Hook should run now!
 export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
