328d1b1ba111b718077f183e49a7ed6ca8869f87
Yannick Wenger <y.wenger@gmx.de>
2025-11-12 15:48:04 +0100

    test: verify husky with typescript file

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// Test comment for Husky hook
 export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
